e6c52126c872dccf41ab1147c576cc96aa9b9e29
Manisha Patel <manisha2@rhel6-64b.slac.stanford.edu>
2016-07-06 13:25:58 -0700

    added code regarding lightmap

--- digitization/chargeDigitizer/src/nEXOLightReadoutDigitize.cxx
+++ digitization/chargeDigitizer/src/nEXOLightReadoutDigitize.cxx
@@ -1,5 +1,6 @@
 #include "nEXOLightReadoutDigitize.hh"
-
+#include <iostream>
+using namespace std;
 nEXOLightReadoutDigitize::nEXOLightReadoutDigitize()
 {
 }
@@ -18,4 +19,21 @@ void nEXOLightReadoutDigitize::Digitize()
 
 void nEXOLightReadoutDigitize::SetNoiseModel()
 {
+}
+bool nEXOLightReadoutDigitize::LoadLightMap(TString lightMapName)
+{
+fFile = new TFile(lightMapName);
+fHist = (TH2D*) fFile->Get("hist");  
+if (fHist == 0) return false; 
+cout << " Entries  " << fHist->GetEntries() << endl;
+return true;
+}
+
+double nEXOLightReadoutDigitize::GetEfficiency(double x, double y, double z)
+{
+double rho = sqrt(x*x+y*y);
+double z0 = z;
+double iBin = fHist->FindBin(rho, z0);
+
+return fHist->GetBinContent(iBin); 
 }
