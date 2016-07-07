667e18bf1d7f9ce451b6bc029926ec730c75a8b5
Manisha Patel <manisha2@rhel6-64g.slac.stanford.edu>
2016-07-07 13:17:25 -0700

    added CalcNPE and PEactive

--- digitization/chargeDigitizer/src/nEXOLightReadoutDigitize.cxx
+++ digitization/chargeDigitizer/src/nEXOLightReadoutDigitize.cxx
@@ -1,5 +1,6 @@
 #include "nEXOLightReadoutDigitize.hh"
 #include <iostream>
+#include "nEXODigiAnalysis.hh"
 using namespace std;
 nEXOLightReadoutDigitize::nEXOLightReadoutDigitize()
 {
@@ -36,4 +37,36 @@ double z0 = z;
 double iBin = fHist->FindBin(rho, z0);
 
 return fHist->GetBinContent(iBin); 
+}
+
+void nEXOLightReadoutDigitize::CalcNPE(Long64_t evtEntry)
+{
+  fNPE = 0;
+  cout << "PE " << fNPE << endl;
+  nEXODigiAnalysis::GetInstance()->GetEntry(evtEntry);
+  Int_t NOP = nEXODigiAnalysis::GetInstance()->GetNumOP();
+  cout << "NOP " << NOP << endl;
+  for (Int_t i=0; i<NOP; i++) {
+    Double_t opX=0., opY=0., opZ=0., opE=0., opT=0.;
+    nEXODigiAnalysis::GetInstance()->GetOP(i, opE, opX, opY, opZ, opT);
+    cout << "i argument " << i << endl;
+    cout << "opX position " << opX << endl;
+    cout << "opY position " << opY << endl;
+    cout << "opZ position " << opZ << endl;
+    
+    Double_t efficiency = nEXOLightReadoutDigitize::GetEfficiency(opX, opY, opZ);  
+    fNPE += efficiency;
+    cout << "efficiency " << efficiency << endl;
+    cout << "PE " << fNPE << endl;
+ 
+    fNPEactive = 0;
+    Double_t rho= sqrt(opX*opX + opY*opY);
+    cout << "rho position " << rho << endl; 
+    if ((-105 <= rho && rho <= 105) && (0 <= opZ && opZ <= 18.16)) {
+    fNPEactive += nEXOLightReadoutDigitize::GetEfficiency(opX, opY, opZ);
+    cout << "PEactive " << fNPEactive << endl;   
+     }	
+    if (i>10) break;
+     	
+ }   
 }
