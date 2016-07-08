933066390d6fcf818b88fe55632bca9a658981d6
Manisha Patel <manisha2@rhel6-64i.slac.stanford.edu>
2016-07-08 11:23:57 -0700

    root script

--- utilities/data/test.C
+++ utilities/data/test.C
@@ -1,12 +1,13 @@
 void test()
 {
-	TFile f("data2.root", "recreate"); //creates a Tree file data2.root
+	 	// dont think this line is needed: hist2 = (TH2D*)f.Get("hist2");
+
+	TFile *data = TFile::Open("~/software/LXe_Test_Stand_Analysis_light_map/utilities/data/data.root"); //Opens existing TFile data.root
+//loop over events in tree, calc ave photon_nr, loop over fill
+TFile f("data2.root", "recreate"); //creates a Tree file data2.root
 	TCanvas c1("c1", "");
 	TH2D *hist2 = new TH2D("hist2", "light map", 40, 0, 8, 40, 0, 8); //Creates new TH2D histogram
- 	// dont think this line is needed: hist2 = (TH2D*)f.Get("hist2");
-
-	TFile *data = TFile::Open("~/software/LXe_Test_Stand_Analysis_light_map/data.root"); //Opens existing TFile data.root
-
+ 
   	TTree *tree = data->Get("data"); //get TTree data from data.root
  	cout << tree->GetEntries() << endl; 
 
@@ -21,10 +22,11 @@ void test()
 	tree->SetBranchAddress("photon_nr",&photon_nr);
 
 	hist2->Fill(zdir,radius,photon_nr);
-	hist2->Fill(zdir,radius,event);
 	cout << hist2->GetEntries() << endl;
 	hist2->Draw("colz");
 	c1.Update();
+	c1.Print("hist2.pdf");
+	hist2->Write();
 	Int_t pause;
 	cin >> pause;
 
